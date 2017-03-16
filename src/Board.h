bdd4610fb038a14f68d0927ff8cfc88ab7a28c22
Artillan <antoine.artillan@telecomnancy.net>
2017-03-16 10:37:33 +0100

    Création du Board.h

---
+++ src/Board.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv);
+
+void displayBoard();
+
+void shutdownBoard();
+
+void updateBoard();
--- src/Board.java
+++
@@ -1,5 +0,0 @@
-/**
- * Créé par antars le 14/03/17.
- */
-public class Board {
-}
