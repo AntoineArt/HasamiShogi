d33a6df678bbd8500f81c1b826e262d04306f25a
Artillan <antoine.artillan@telecomnancy.net>
2017-04-04 15:45:46 +0200

    Setup d'une fenêtre SDL

--- src/App.c
+++ src/App.c
@@ -1,15 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "Board.h"
-#include <SDL/SDL.h>
+#include "App.h"
 
 
 int main(int argc,char* argv[]){
-  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO); //Démarage SDL 
-  SDL_SetVideoMode(640,480,8,SDL_DOUBLEBUF);
-  
-  
+  //Initialisation simple
+  if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    fprintf(stdout, "Echec de l'initialisation de la SDL (%s)\n", SDL_GetError());
+    return -1;
+  }
+
+  //Création de la fenêtre
+  SDL_Window* pWindow = NULL;
+  pWindow = SDL_CreateWindow("Test Fenêtre",SDL_WINDOWPOS_UNDEFINED,
+                                            SDL_WINDOWPOS_UNDEFINED,
+                                            640,
+                                            480,
+                                            SDL_WINDOW_SHOWN);
+
+  //Test d'affichage
+  if (pWindow){
+    SDL_Delay(3000); //On attend 3s que l'utilisateur voie la fenêtre
+    SDL_DestroyWindow(pWindow);
+  }
+  else{
+    fprintf(stderr, "Erreur de création de la fenêtre : %s\n", SDL_GetError() );
+  }
+
   SDL_Quit(); //Arrêt Propre
   return 0;
 }
