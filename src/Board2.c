534cdafaaf126fe44e03d22328dda22c510b2920
Corentin <corentin.jacquet@telecomnancy.net>
2017-05-01 18:05:39 +0200

    update the Board and create the Game and IA files

--- src/Board2.c
+++ src/Board2.c
@@ -2,50 +2,31 @@
 
 matrice allocateBoard()
 {
-	matrice m;
+	board m;
 }
 
-void resetBoard(matrice m){
-	for(i=0;i<=9;i++){
-		for(j=0;j<=1;j++){
-			m.board[j][i]=1; //Pieces du J1
-		}
-		for(j=2;j<=7;j++){
-			m.board[j][i]=0;
-		}
-		for(j=8;j<=9;j++){
-			m.board[j][i]=2; //Pieces du J2
-		}
-	}
-}
 
-void write(int status, coordonnees c)
+void write(int status, coordonates c)
 {
-	m.board[c.x][c.y]=status;
+	m.board[c.x][c.y] = status;
 }
 
 void movePiece(c1, c2)
 {
-	write(m.board[c.x][c.y],c2);
+	write(m.board[c1.x][c1.y],c2);
 	write(0,c1);
 }
 
-void furtherUpdate(int joueur, coordonnees c2)
+void catchPiece(int currentPlayer, coordonates c2) 
 {
 	
 }
 
-bool mouvementValide(int joueur, coordonnees c1, coordonnees c2)
+void updateBoard(int currentPlayer, coordonates c1, coordonates c2)
 {
-	bool boo = true;
-	return boo
-}
-
-void updateBoard(int joueur, coordonnees c1, coordonnees c2)
-{
-	if(mouvementValide(c1,c2)){
+	if(checkMovement(c1,c2)){
 		movePiece(c1, c2);
-		furtherUpdate(joueur,c2);
+		catchPiece(currentPlayer,c2);
 	}
 	else{printf("Invalid Movement!")}
 }
