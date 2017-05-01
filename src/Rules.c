8ade225c3f248bab6eb76a9109394b95669d2f8d
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-05-01 16:15:26 +0200

    m.a.j. Rules

--- src/Rules.c
+++ src/Rules.c
@@ -1,1 +1,36 @@
 #include Rules.h
+
+void resetBoard(matrice m){
+	for(i=0;i<=9;i++){
+		for(j=0;j<=1;j++){
+			m.board[j][i]=1; //Pieces du J1
+		}
+		for(j=2;j<=7;j++){
+			m.board[j][i]=0;
+		}
+		for(j=8;j<=9;j++){
+			m.board[j][i]=2; //Pieces du J2
+		}
+	}
+}
+
+int checkVictory()
+{
+	if ()
+}
+
+void checkCatch()
+{
+
+}
+
+bool checkMovement(coordonates c1, coordonates c2)
+{
+	bool boo = true;
+	//Check si case différente
+	//Check déplacement ligne ou colonne
+	//Check si quelqu'un sur le chemim
+		//Si var=1, check si c'est un saut
+	return boo
+}
+
--- src/Rules.h
+++ src/Rules.h
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/*
+	Return the player who won this turn (0 if none of them)
+*/
+int checkVictory()
+
+/*
+	Return the list of the coordonates
+*/
+void checkCatch(c1, c2);
+
+/*
+	Check if a move is valid
+*/
+bool checkMovement(int currentPlayer, coordonates c1, coordonates c2);
