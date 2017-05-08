0e5f5ebad4d82901b117b599708e2031d51305af
Corentin <corentin.jacquet@telecomnancy.net>
2017-05-08 20:27:26 +0200

    debug

--- src/Game.h
+++ src/Game.h
@@ -1,9 +1,11 @@
+#include "Board.c"
 #include "IA.c"
 
 typedef struct {
 	int gameMode; //the gameMode ie player vs player 1, player vs computer 2, computer vs player 3,  computer vs computer (watch mode) 4
-  int var; // variante of ashami shogi beeing played
-  } game;
+	int var; // variante of ashami shogi beeing played
+	board b; // board of the game
+} game;
 
 
 /*
