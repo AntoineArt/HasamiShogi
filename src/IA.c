7272d41b6138e9fb695e3853445b6b2afee8356a
Artillan <antoine.artillan@telecomnancy.net>
2017-05-29 19:15:24 +0200

    Merge

--- src/IA.c
+++ src/IA.c
@@ -89,7 +89,7 @@ double alphabeta(Game *g, Tree *P, int depth, double a, double b, int player) { 
 				break; // α cut-off
 			}*/
 		}
-		
+
 		P->value=min; //saving the value
 		/*
 		for (i=0; i<(16*18); i++) {
@@ -122,7 +122,7 @@ void buildTree(Game* g, int depth, Tree *dad, int player) {
 				(dad->sons[dad->nbofSons])->nbofSons = 0; //initialize the value
 				//recursive call
 				buildTree(g, (depth-1), (dad->sons[dad->nbofSons]), (3-player)); //creates the subtree of the new son
-				//RECURSION DOES NOT WORK AS EXPECTED : ONLY LAUNCH ONCE INSTEAD OF DEPTH 
+				//RECURSION DOES NOT WORK AS EXPECTED : ONLY LAUNCH ONCE INSTEAD OF DEPTH
 				(dad->nbofSons)++; //incremente the son number accordingly
 				movePiece(g, moves[j], c1);
 			}
@@ -174,16 +174,16 @@ double evaluate(Game *g, int player) {
 	double res;
 	res = 0;
 	//attribuate value
-	
+
 	//having more tokens
 	int friendTokenNb = g->currentPlayer==1 ? g->countPlayer1 : g->countPlayer2 ; //nb of tokens of the currentPlayer
 	int ennemyTokenNb = g->currentPlayer==1 ? g->countPlayer2 : g->countPlayer1 ; //nb of tokens of the currentPlayer
 	res = res + (100 * (friendTokenNb - ennemyTokenNb)); //ones wants to have more token
-	
+
 	//having aligned tokens including victory condition
 	res = res + 10 * nbofLigns(g, player);
-	res = res - 10 * nbofLigns(g, 3-player); 
-	
+	res = res - 10 * nbofLigns(g, 3-player);
+
 	//return the value
 	printf("res val : %lf \n",res);
 	return res;
@@ -206,14 +206,14 @@ int nbofLigns(Game* g, int player) {
 				if (last) {nb++;}
 				else { nb=1;}
 			} else { //means end of an alignment
-				if (nb==3) 
+				if (nb==3)
 				{ mem++; }
-				else if (nb==4) 
+				else if (nb==4)
 				{ mem = mem + 10; }
-				else if (nb>=5) 
+				else if (nb>=5)
 				{ mem = mem + 100; }
 			}
-		}		
+		}
 	}
 	//checking Left diags
 	for (i=4; i<15 ;i++) { //diag sum
@@ -227,11 +227,11 @@ int nbofLigns(Game* g, int player) {
 				if (last) {nb++;}
 				else { nb=1;}
 			} else { //means end of an alignment
-				if (nb==3) 
+				if (nb==3)
 				{ mem++; }
-				else if (nb==4) 
+				else if (nb==4)
 				{ mem = mem + 10; }
-				else if (nb>=5) 
+				else if (nb>=5)
 				{ mem = mem + 100; }
 			}
 		}
@@ -248,11 +248,11 @@ int nbofLigns(Game* g, int player) {
 				if (last) {nb++;}
 				else { nb=1;}
 			} else { //means end of an alignment
-				if (nb==3) 
+				if (nb==3)
 				{ mem++; }
-				else if (nb==4) 
+				else if (nb==4)
 				{ mem = mem + 10; }
-				else if (nb>=5) 
+				else if (nb>=5)
 				{ mem = mem + 100; }
 			}
 		}
@@ -284,25 +284,25 @@ double evaluate(Game *g, Coordinates c1, Coordinates c2) {
 	Coordinates* tab = checkCatch(g, c2); //the tab of to-be-caught-by-this-play tokens
 	res = res + (tab[0].x-1) * 100; //-1 because of the false first coordinates
 	free(tab);
-	
+
 	int friendTokenNb = g->currentPlayer==1 ? g->countPlayer1 : g->countPlayer2 ; //nb of tokens of the currentPlayer
 	int ennemyTokenNb = g->currentPlayer==1 ? g->countPlayer2 : g->countPlayer1 ; //nb of tokens of the currentPlayer
 
 	//having more tokens
 	res = res + (100 * (friendTokenNb - ennemyTokenNb)); //ones wants to have more token
 
-	
+
 	//be close to ennemy
 	res = res + (15 * nbofEnnemy(g, c2));
 	//but also close to friends
 	res = res - 20 * nbofFriends(g, c2);
-	
+
 	// X shape is strong
 	res = res + 12 * nbXShape(g, c2);
-				
+
 	// / or \ or | shapes are strong
 	res = res + 45 * nbLignShape(g, c2);
-			
+
 	// _ shape is weak
 	res = res - 30 * nb_Shape(g, c2);
 
@@ -315,7 +315,7 @@ double evaluate(Game *g, Coordinates c1, Coordinates c2) {
 
 	//if protect friends then good
 	//I don't fucking know how to implement this
-	
+
 	//reverse the play
 	movePiece(g, c2, c1); //a ashami shogi play can always be reversed
 	return res;
